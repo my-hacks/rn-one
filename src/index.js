9cb07125904b35a93067ee349606f57181584480
Emerson Silva <emerson_jdss@hotmail.com>
2020-08-15 13:39:27 -0300

    config default index

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
+
+function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Emerson</Text>
+      </SafeAreaView>
+    </>
+  );
+}
+
+export default App;
